465b960b21a182277e5941e495c03a1d2dbf8316
Chris Creighton (@creighcl) <27293513+Creighcl@users.noreply.github.com>
2020-10-05 15:46:27 -0400

    flip

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ function App() {
   return (
     <div className="App">
       <div className="side-piece">
-        <h3>Play In Browser on Itch.io</h3>
-        <a href="https://creighcl.itch.io/citdev-ld-47">https://creighcl.itch.io/citdev-ld-47</a>
-      </div>
-
-      <div className="side-piece">
         <h3>Leaderboards on Infamy.dev</h3>
         <a href="https://infamy.dev/highscore/web?id=bjorn-to-dance">https://infamy.dev/highscore/web?id=bjorn-to-dance</a>
+      </div>
+      <div className="side-piece">
+        <h3>Play In Browser on Itch.io</h3>
+        <a href="https://creighcl.itch.io/citdev-ld-47">https://creighcl.itch.io/citdev-ld-47</a>
       </div>
     </div>
   );
